00a2380fbf90a0a8df9f383924451e97f1728199
Anmol59 <anmoltiwari817@gmail.com>
2026-03-13 20:29:14 +0530

    FInd FirstNonRepeating letter in a string of characters

---
+++ Queue/non_repeating_letter.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+void FirstnonReapeating(string str){ //o(n) o(n)
+    queue<char> Q;
+    int freq[26]={0};
+
+    for(int i=0;i<str.size();i++){
+        char ch= str[i];
+         Q.push(ch);
+        freq[ch-'a']++;
+
+
+        while(!Q.empty() && freq[Q.front()-'a']>1){
+            Q.pop();
+        }
+
+        if(Q.empty()){
+            cout<<"-1\n";
+        }else{
+            cout<<Q.front()<<"\n";
+        }
+       
+
+    }
+}
+int main(){
+    string str="aabccxb";
+    FirstnonReapeating(str);
+
+    
+    return 0;
+}
