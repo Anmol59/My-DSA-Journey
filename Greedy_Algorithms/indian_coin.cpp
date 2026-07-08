2a8118fb3fe5e520bd05625830622ad3874a2794
Anmol59 <anmoltiwari817@gmail.com>
2026-07-08 22:03:47 +0530

    greedy algos

---
+++ Greedy_Algorithms/indian_coin.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int getMinChange(vector<int>coins,int v ){
+    int ans =0;
+    int n = coins.size();
+
+    for(int i = n-1; i>=0 && v>0; i--){
+        if(v >= coins[i]){
+            ans +=v/ coins[i];
+            v=v%coins[i];
+        }
+    }
+    cout<<"min coins for change= "<<ans<<endl;
+    return ans;
+}
+
+int main(){
+ vector<int> coins= {1,2,5,10,20,50,100,500,2000};
+
+ int v =1099;
+ getMinChange(coins, v);
+
+    
+    return 0;
+}
